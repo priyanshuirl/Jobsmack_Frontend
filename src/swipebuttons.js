9f4342802832041a544fb15393ba036a3cf0d167
Priyanshu Mishra <priyanshu12651265@gmail.com>
2021-09-21 23:57:44 +0530

    added swipe notice and fixed oveflow error

--- src/swipebuttons.js
+++ src/swipebuttons.js
@@ -8,22 +8,25 @@ import FlashOnIcon from '@mui/icons-material/FlashOn';
 import { IconButton } from "@mui/material";
 function SwipeButtons() {
     return (
-        <div className="swipeButtons" onClick={() => { window.location.reload(); }}>
-            <IconButton className="swipeButtons__repeat">
-                <ReplayIcon fontSize="large" />
-            </IconButton>
-            <IconButton className="swipeButtons__left">
-                <CloseIcon fontSize="large" />
-            </IconButton>
-            <IconButton className="swipeButtons__star">
-                <StarRateIcon fontSize="large" />
-            </IconButton>
-            <IconButton className="swipeButtons__right">
-                <FavoriteIcon fontSize="large" />
-            </IconButton>
-            <IconButton className="swipeButtons__lightning">
-                <FlashOnIcon fontSize="large" />
-            </IconButton>
+        <div>
+            <p className="instrc">Swipe Right/Left on the cards to see more job listings.</p>
+            <div className="swipeButtons" onClick={() => { window.location.reload(); }}>
+                <IconButton className="swipeButtons__repeat">
+                    <ReplayIcon fontSize="large" />
+                </IconButton>
+                <IconButton className="swipeButtons__left">
+                    <CloseIcon fontSize="large" />
+                </IconButton>
+                <IconButton className="swipeButtons__star">
+                    <StarRateIcon fontSize="large" />
+                </IconButton>
+                <IconButton className="swipeButtons__right">
+                    <FavoriteIcon fontSize="large" />
+                </IconButton>
+                <IconButton className="swipeButtons__lightning">
+                    <FlashOnIcon fontSize="large" />
+                </IconButton>
+            </div>
         </div>
     )
 }
